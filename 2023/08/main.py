474a76c1f8ddef7399acc7349b529518a3e874f0
Simon Büscher <sbuescher@brockhaus-ag.de>
2023-12-08 11:17:54 +0100

    2023 day 8

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -8,7 +8,7 @@ def get_puzzle_input():
     return list(lines[0].strip()), {line[0:3]: (line[7:10], line[12:15]) for line in lines[2:]}
 
 
-def run(instructions, graph, start, end):
+def run(start, end):
     current = start
     for i, instruction in enumerate(itertools.cycle(instructions), start=1):
         current = graph[current][list("LR").index(instruction)]
@@ -17,6 +17,6 @@ def run(instructions, graph, start, end):
 
 
 if __name__ == "__main__":
-    inst, g = get_puzzle_input()
-    print(f"Puzzle 1: {run(inst, g, 'AAA', lambda x: x == 'ZZZ')}")  # Puzzle 1: 13301
-    print(f"Puzzle 2: {math.lcm(*[run(inst, g, n, lambda x: x.endswith('Z')) for n in g.keys() if n.endswith('A')])}")  # Puzzle 2: 7309459565207
+    instructions, graph = get_puzzle_input()
+    print(f"Puzzle 1: {run('AAA', lambda x: x == 'ZZZ')}")  # Puzzle 1: 13301
+    print(f"Puzzle 2: {math.lcm(*[run(n, lambda x: x.endswith('Z')) for n in graph.keys() if n.endswith('A')])}")  # Puzzle 2: 7309459565207
