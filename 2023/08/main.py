197f1af716b350cb5ed0fd077751a27292d316eb
Simon Büscher <sbuescher@brockhaus-ag.de>
2023-12-08 07:42:36 +0100

    2023 day 8

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -17,7 +17,6 @@ def run(instructions, graph, start, end):
 
 
 if __name__ == "__main__":
-    instructions, graph = get_puzzle_input()
-    print(f"Puzzle 1: {run(instructions, graph, 'AAA', lambda x: x == 'ZZZ')}")  # Puzzle 1: 13301
-    print(
-        f"Puzzle 2: {math.lcm(*[run(instructions, graph, node, lambda x: x.endswith('Z')) for node in graph.keys() if node.endswith('A')])}")  # Puzzle 2: 7309459565207
+    inst, g = get_puzzle_input()
+    print(f"Puzzle 1: {run(inst, g, 'AAA', lambda x: x == 'ZZZ')}")  # Puzzle 1: 13301
+    print(f"Puzzle 2: {math.lcm(*[run(inst, g, n, lambda x: x.endswith('Z')) for n in g.keys() if n.endswith('A')])}")  # Puzzle 2: 7309459565207
